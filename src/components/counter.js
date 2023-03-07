0a9a28f768e6d1ff05a306c334a4e970116115da
Fernando <21fernandinhow@gmail.com>
2023-03-07 19:49:09 -0300

    change text

--- src/components/counter.js
+++ src/components/counter.js
@@ -13,7 +13,7 @@ function Counter() {
             <div className="main">
                 <img className="trophy" src='https://png.pngtree.com/png-vector/20221117/ourmid/pngtree-fifa-world-cup-trophy-clipart-illustration-png-image_6455581.png' alt='trophy' />
                 <h1>Faltam {difference} dias para a copa! </h1>
-                <p>A próxima copa do mundo será sediada nos EUA, Canadá em México, com início em 6 de Junho de 2026.</p>
+                <p>A próxima copa do mundo será sediada nos EUA, Canadá em México, com início em 3 de Junho de 2026.</p>
             </div>
         </div>
     )
