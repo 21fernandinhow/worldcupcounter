7dbd6cb758e3b0b8b801e41d204379b5361313da
21fernandinhow <21fernandinhow@gmail.com>
2024-01-24 18:13:31 -0300

    Just insert a word that was missing

--- src/components/shareBtn.js
+++ src/components/shareBtn.js
@@ -1,14 +1,14 @@
 import { HiShare } from "react-icons/hi";
 
 export default function ShareBtn (props) {
-    const message = `Faltam ${props.days} para a Copa do Mundo! Acompanhe a contagem regressiva em: https://quantofaltapracopa.netlify.app/`
+    const message = `Faltam ${props.days} dias para a Copa do Mundo! Acompanhe a contagem regressiva em: https://quantofaltapracopa.netlify.app/`
 
     return (
         <a 
             id="shareBtn" 
             target="_blank" 
             rel="noopener noreferrer" 
-            href={`https://wa.me/send?text=${encodeURIComponent(message)}`}
+            href={`https://wa.me/?text=${encodeURIComponent(message)}`}
         >
             <HiShare />
         </a>
